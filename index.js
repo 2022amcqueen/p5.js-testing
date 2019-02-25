b9d76f41ca1f94888d7e46093c783b4ec31ba741
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2019-02-25 15:12:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-let i = 0
-
 function setup() {
     createCanvas(600, 600, WEBGL)
     background(51)
 }
 
 function draw() {
-    sphere(150, 5)
-    translate(width / 2, 0, height / 2)
-    rotateX(i)
+    normalMaterial()
+    sphere(150, 3)
     
-    i++
+    translate(width / 2, 0, height / 2)
+    rotate(frameCount)
 }
