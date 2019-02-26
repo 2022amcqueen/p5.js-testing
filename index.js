29a77f6e58df2d261a7eafa2248a9f714b1251aa
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2019-02-26 11:13:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,23 +10,19 @@ function draw() {
 	//normalMaterial()
 
 
-	//rotateX(frameCount * rate)
-	//rotateY(frameCount * rate)
-	//rotateZ(frameCount * rate)
+	rotateX(frameCount * rate)
+	rotateY(frameCount * rate)
+	rotateZ(frameCount * rate)
 	
 	for(let x = 0; x < 100; x++) {
 		for(let z = 0; z < 100; z++) {
 			//push()
-				rotateX(frameCount * rate)
-				rotateY(frameCount * rate)
-				rotateZ(frameCount * rate)
+			box(50, 50, 50)
 			
-				box(150, 150, 150)
-			
-				translate(x * 150, 0, z * 150)
+			translate(x * 150, 0, z * 150)
 			//pop()
 		}
 	}
 
-	translate(width / 2, 0, height / 2)
+	//translate(width / 2, 0, height / 2)
 }
