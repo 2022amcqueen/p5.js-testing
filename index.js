ee410814936544460ec3bfc636d1c00f2003ae94
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2019-02-26 11:08:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,17 +13,15 @@ function draw() {
 	//rotateX(frameCount * rate)
 	//rotateY(frameCount * rate)
 	//rotateZ(frameCount * rate)
-
-	push() // new scope
-
-		for(let x = 0; x < 100; x++) {
-			for(let z = 0; z < 100; z++) {
+	
+	for(let x = 0; x < 100; x++) {
+		for(let z = 0; z < 100; z++) {
+			push()
 				box(150, 150, 150)
 				translate(x * 150, 0, z * 150)
-			}
+			pop()
 		}
-
-	pop()
+	}
 
 	translate(width / 2, 0, height / 2)
 }
