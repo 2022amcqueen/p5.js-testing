047b5e451b0ec58f08eaefcdb517b0411959ac4d
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2019-02-26 11:20:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function draw() {
 	for(let x = 0; x < 10; x += 50) {
 		for(let y = 0; y < 10; y += 50) {		
 			push()
-				box(50, 50, 50)	
 				translate(x, y, 0)
+				box(50, 50, 50)	
 			pop()
 		}
 	}
