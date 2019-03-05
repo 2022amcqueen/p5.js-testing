76ebf24eda6d9c3c60f52314f6555578be431f65
Andrew McQueen <32377685+2022amcqueen@users.noreply.github.com>
2019-03-05 14:37:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,16 @@ function setup() {
 function draw() {
 	let rate = .01
 	
-	let xAngle = Math.cos(mouseX)
-	let yAngle = Math.sin(mouseY)
-	
 	background(200)
 	
 	for(let x = 0; x < 1000; x += 50) {
 		for(let z = 0; z < 1000; z += 50) {
-			//push()
+			push()
 				let y = noise(x * 0.01, z * 0.01)
 				
-				
-				
-				translate(50, y * 50, 50)
+				translate(x, y * 50, z)
 				box(50, 50, 50)
-			//pop()
+			pop()
 		}
 	}
 	
